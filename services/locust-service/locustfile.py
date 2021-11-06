e6ece068f3901deafc44ca8858b060a6c2897a44
emsesc <emseschen@gmail.com>
2021-11-05 21:49:19 -0400

    so far

--- services/locust-service/locustfile.py
+++ services/locust-service/locustfile.py
@@ -2,18 +2,7 @@ import random
 from locust import HttpUser, TaskSet, between
 
 hats = [
-  'baby',         'bucket',      'beach',
-  'spinner',      'cartoon',     'skull',
-  'blob',         'santa',       'st-patricks',
-  'santa',        'graduation',  'blob',
-  'pirate',       'pokemon',     'clown',
-  'spy',          'st-patricks', 'mario',
-  'tophat',       'pilot',       'tophat',
-  'construction', 'beach',       'poop',
-  'turkey',       'pepe',        'st-patricks',
-  'shark',        'alien',       'penguin',
-  'tinyhat',      'cat-ears',    'spicy',
-  'PIXIE'
+  'cat-ears'
 ]
 
 def index(l):
